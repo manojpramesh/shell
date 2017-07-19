61ad93ab9c2b920be877d879fd437451122aac0a
Manoj P R <manojpramesh@gmail.com>
2017-07-19 15:13:12 +0530

    comment to run from github

--- ethereum_docker.sh
+++ ethereum_docker.sh
@@ -3,12 +3,16 @@
 # pre-requisites
 # bash <(curl -s https://raw.githubusercontent.com/manojpramesh/shell/master/server_init.sh)
 
+# Run from github
+# bash <(curl -s https://raw.githubusercontent.com/manojpramesh/shell/master/ethereum_docker.sh)
+
 # Get docker from github
 git clone https://github.com/manojpramesh/ethereum-docker.git
 
 # Start ethereum nodes
 cd ethereum-docker
 sudo docker-compose up -d
+cd ..
 
 # Show status
 sudo docker ps
