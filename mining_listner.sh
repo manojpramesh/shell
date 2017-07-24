f6635e4e6d9ca1b6b1a2fc74ef4a6b3bb7544c3a
Manoj P R <manojpramesh@gmail.com>
2017-07-24 18:51:18 +0530

    mining listner

---
+++ mining_listner.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Run from github
+# bash <(curl -s https://raw.githubusercontent.com/manojpramesh/shell/master/mining_listner.sh)
+
+# Open container
+docker exec -it bootstrap bash
+
+# Attach to geth JavaScript console
+geth attach ipc://root/.ethereum/devchain/geth.ipc
+
+# run mining listner
+function checkWork() {
+    if (txpool.status.pending > 0) {
+        console.log("Mining started...");
+        if (eth.mining) return;
+        miner.start(1);
+        console.log(JSON.stringify(txpool.inspect.pending));
+    } else {
+        miner.stop();
+        console.log("Mining stopped.");
+    }
+}
+eth.filter("latest", function(err, block) { checkWork(); });
+eth.filter("pending", function(err, block) { checkWork(); });
+checkWork();
