d019bea43c6b138bfe963ce6b5d51d9a7f1caae8
Manoj P R <manojpramesh@gmail.com>
2017-11-27 17:54:28 +0530

    update docker installer

--- server_init.sh
+++ server_init.sh
@@ -18,10 +18,13 @@ sudo apt-get update
 sudo apt-get install -y mongodb-org
 
 # Install Docker CE
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get install -y docker-ce
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu zesty stable"
+sudo apt-get update
+sudo apt-get install docker-ce
 
 # Executing the Docker Command Without Sudo
 # sudo usermod -aG docker ${USER}
