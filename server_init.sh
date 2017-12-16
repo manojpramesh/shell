637d34021a129ac54bc562ed25946b364e04297c
Manoj <manojpramesh@gmail.com>
2017-12-16 22:53:18 +0530

    prompt yes response

--- server_init.sh
+++ server_init.sh
@@ -19,12 +19,12 @@ sudo apt-get install -y mongodb-org
 
 # Install Docker CE
 sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+yes Y | sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu zesty stable"
 sudo apt-get update
-sudo apt-get install docker-ce
+yes Y | sudo apt-get install docker-ce
 
 # Executing the Docker Command Without Sudo
 # sudo usermod -aG docker ${USER}
